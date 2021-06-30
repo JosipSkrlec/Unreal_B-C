fb129bf1cbcc2568657a9162483dc579d9cc5eaa
JosipSkrlec <jskrlec1997@gmail.com>
2021-06-30 22:00:08 +0200

    Update, added pseudo code to .cpp file

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,25 +4,48 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    HiddenWord = TEXT("Cake");
 
-    PrintLine(TEXT("Welcome to BC game!"));
-    PrintLine(TEXT("Guess the 4 letter word!")); // magic word , REMOVE it!
-    PrintLine(TEXT("Press TAB to continue"));
+    // Welcoming The Player
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
+    PrintLine(TEXT("Press enter to continue..."));
+
+    // Setting Up Game
+    HiddenWord = TEXT("cake"); // Set the HiddenWord
+    // Set Lives
+
+    // Prompt Player For Guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
 
-    if (Input == "") {
-        return;
-    }
+    // Checking PlayerGuess
 
-    if (Input == HiddenWord) {
+    if (Input == HiddenWord)
+    {
         PrintLine(TEXT("You have Won!"));
     }
-    else {
-        PrintLine(TEXT("You have Lose!"));
+    else
+    {
+        PrintLine(TEXT("You have Lost!"));
+
+
     }
+    // Check If Isogram
+    // Prompt To Guess Again
+    // Check Right Number Of Characters
+    // Prompt To Guess Again
+
+    // Remove Life
+
+    // Check If Lives > 0
+    // If Yes GuessAgain
+    // Show Lives Left
+    // If No Show GameOver and HiddenWord?
+    // Prompt To Play Again, Press Enter To Play Again?
+    // Check User Input
+    // PlayAgain Or Quit
+
 }
