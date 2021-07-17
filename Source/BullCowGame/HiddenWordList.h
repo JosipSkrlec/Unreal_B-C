f47c3ee8958ba11305db610d6cef85427b2a0111
JosipSkrlec <jskrlec1997@gmail.com>
2021-07-17 15:59:18 +0200

    Added txt file versioning

---
+++ Source/BullCowGame/HiddenWordList.h
@@ -0,0 +1,19 @@
+// napravljeno je u BullCowCatriage citanje iz Text file-a koji se nalazi u contentu pod WordLists folderom
+
+// ako zelimo imati dinamicni .txt -> TArray a ne hard kodirano u TArray
+//#include<CoreMinimal.h>
+//const TArray<int32> Numbers =
+//{
+//	1, 
+//	2, 
+//	3, 
+//	4
+//};
+//const TArray<FString> Words =
+//{
+//	TEXT("Cakes"),
+//	TEXT("Water"),
+//	TEXT("Plums"),
+//	TEXT("Kings"),
+//	TEXT("Knife"),
+//};
